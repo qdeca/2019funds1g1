e6521e08b8263c8c073fb6404ae061cff6756f78
Decayeux <qdecayeux@natsystem.fr>
2019-04-29 20:01:15 +0200

    added instancation of classes

--- Geometry/src/com/epita/geometry/tests/Main.java
+++ Geometry/src/com/epita/geometry/tests/Main.java
@@ -1,15 +1,51 @@
 package com.epita.geometry.tests;
 
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import com.epita.geometry.entities.Circle;
+import com.epita.geometry.entities.Rectangle;
+
 public class Main {
 	
 	public static void main(String[] args) {
 		
-		// create a circle
-		// take user input to determine the radius
-		// display area and perimeter
-		
-		// create a rectangle
-		// take user input to determine height and length
-		// display whether it is a square or not
+		Scanner scanner = new Scanner(System.in);
+		try {
+			double radius = scanner.nextDouble(); 		
+			// take user input to determine the radius
+			Circle circle = new Circle(radius); 		
+			// create a circle
+			double area = circle.getArea();
+			System.out.printf("Circle's area : %.2f \n", area);
+							// % corresponds to a parameter in the printf
+							// argument (here, it's area)
+							// .2f means that only 2 numbers after the comma
+							// are displayed
+			// display area
+			System.out.println("Circle's perimeter : " + circle.getPerimeter());
+			// display perimeter
+
+
+			
+			double height = scanner.nextDouble();
+			double length = scanner.nextDouble();
+			Rectangle rectangle = new Rectangle(height, length);
+			if  (height == length) {
+				System.out.println("This rectangle is a square");
+			} else {
+				System.out.println("This rectangle is not a square");
+			}
+			
+			
+			
+		} catch(InputMismatchException e) {
+			System.out.println("Please type a valid number");
+		} 
+		finally {
+			scanner.close();
+		}
+
 	}
 }
