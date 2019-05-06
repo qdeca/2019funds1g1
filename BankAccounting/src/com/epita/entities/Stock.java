27677bba115f7210f9d2d162407cc260037c7458
Decayeux <qdecayeux@natsystem.fr>
2019-05-06 19:35:22 +0200

    removed formatting

--- BankAccounting/src/com/epita/entities/Stock.java
+++ BankAccounting/src/com/epita/entities/Stock.java
@@ -1,5 +1,16 @@
 package com.epita.entities;
 
+import java.util.List;
+
 public class Stock {
 
+	private List<StockOrder> stockOrders;
+	
+	public List<StockOrder> getStockOrders() {
+		return stockOrders;
+	}
+
+	public void setStockOrders(List<StockOrder> stockOrders) {
+		this.stockOrders = stockOrders;
+	}
 }
