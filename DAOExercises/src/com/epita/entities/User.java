ed72c2f7a782b808f4f768f29926840143ef9dce
Decayeux <qdecayeux@natsystem.fr>
2019-05-13 19:02:26 +0200

    initial import

---
+++ DAOExercises/src/com/epita/entities/User.java
@@ -0,0 +1,49 @@
+package com.epita.entities;
+
+import java.util.Date;
+
+public class User {
+
+	private String name;
+	
+	private double height;
+	
+	private String adress;
+	
+	private Date birthdate;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+	public String getAdress() {
+		return adress;
+	}
+
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+
+	public Date getBirthdate() {
+		return birthdate;
+	}
+
+	public void setBirthdate(Date birthdate) {
+		this.birthdate = birthdate;
+	}
+	
+	
+	
+}
---
+++ DAOExercises/src/com/epita/tests/FileMain.java
@@ -0,0 +1,10 @@
+package com.epita.tests;
+
+public class FileMain {
+	
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
