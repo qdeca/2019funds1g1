117b5ecf45cd8e07151a046eb982f0bb45ba74ea
Decayeux <qdecayeux@natsystem.fr>
2019-05-13 19:13:30 +0200

    added db test

--- DAOExercises/src/com/epita/entities/User.java
+++ DAOExercises/src/com/epita/entities/User.java
@@ -1,5 +1,6 @@
 package com.epita.entities;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class User {
@@ -11,6 +12,15 @@ public class User {
 	private String adress;
 	
 	private Date birthdate;
+	
+	
+
+	public User(String name, double height, String adress, Date birthdate) {
+		this.name = name;
+		this.height = height;
+		this.adress = adress;
+		this.birthdate = birthdate;
+	}
 
 	public String getName() {
 		return name;
@@ -42,6 +52,11 @@ public class User {
 
 	public void setBirthdate(Date birthdate) {
 		this.birthdate = birthdate;
+	}
+
+	@Override
+	public String toString() {
+		return "User [name=" + name + ", height=" + height + ", adress=" + adress + ", birthdate=" + new SimpleDateFormat("yyyy/MM/yy").format(birthdate) + "] \n";
 	}
 	
 	
