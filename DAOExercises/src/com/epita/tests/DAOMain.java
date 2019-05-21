57144ee97f0e340f045e219f84a501a166a78c13
Decayeux <qdecayeux@natsystem.fr>
2019-05-21 19:48:36 +0200

    added user creation

--- DAOExercises/src/com/epita/tests/DAOMain.java
+++ DAOExercises/src/com/epita/tests/DAOMain.java
@@ -9,16 +9,25 @@ import java.sql.SQLException;
 public class DAOMain {
 	
 	public static void main(String[] args) {
+		//readUsers();
+		createUser();
+		
+		// TO READ   : SELECT
+		// TO CREATE : INSERT INTO
+		// TO DELETE : DELETE FROM
+		// TO UPDATE : UPDATE 
+	}
+
+	private static void readUsers() {
 		try {
 			
 			// CREATE TABLE USER(ID int primary key, NAME varchar(255), EMAIL varchar(255)
 			// INSERT INTO USER VALUES (1, 'Testname', 'Testmail')
 			
-			Connection connection = DriverManager.getConnection("jdbc:h2:tcp://localhost:9093//C:/Formation2019/db/h2DS", "sa", "");
+			Connection connection = DriverManager.getConnection("jdbc:h2:C:/Formation2019/db/h2DS", "sa", "");
 			PreparedStatement statement = connection.prepareStatement("SELECT * FROM USER WHERE ID=1");
-			
-			
 			ResultSet result = statement.executeQuery();
+
 			while(result.next()) {  // for all users in database
 				int id = result.getInt(1); // get id (first column)
 				String name = result.getString(2); // get name (second column)
@@ -30,12 +39,20 @@ public class DAOMain {
 		} catch (SQLException e) {
 			System.out.println("Connection failed : " + e.getMessage());
 		}
-	
-		
-		// TO READ   : SELECT
-		// TO CREATE : INSERT INTO
-		// TO DELETE : DELETE FROM
-		// TO UPDATE : UPDATE 
 	}
 
+	
+	private static void createUser() {
+		try {
+			Connection connection = DriverManager.getConnection("jdbc:h2:C:/Formation2019/db/h2DS", "sa", "");
+			PreparedStatement statement = connection.prepareStatement("INSERT INTO USER VALUES(?,?,?)"); // ? are parameters
+			statement.setInt(1, 5); // first parameter is the value 5
+			statement.setString(2, "NewName"); // second parameter is the value NewName
+			statement.setString(3, "NewMail"); // third parameter is the value NewMail
+			statement.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
 }
